906a7c18f6383e8ef8e50d941c309a46cc73fc10
Adam <a19224@yahoo.com.tw>
2016-08-08 07:19:42 +0000

    add 前端 and 後端

---
+++ bank.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+    <div>
+        <form  method="post" action="sever.php">
+            <table style="text-align:center;margin:0 auto;">
+                <tr>
+                    <td>
+                        <h1>
+                            使用者你好
+                        </h1>
+                    </td>
+                </tr>    
+                <tr>
+                    <td>
+                        <input type="submit" name="btnImport" value="匯入"/>
+                        <input type="submit" name="btnExport" value="匯出" />
+                        <input type="submit" name="btnCheck" value="查看餘額"/>
+                        <input type="submit" name="btnDetails" value="帳目明細"/>
+                    </td>
+                </tr>
+            </table>
+        </form>
+</body>
+</html>
---
+++ sever.php
@@ -0,0 +1,5 @@
+<?php
+    function import(){
+        echo "OK";
+    }
+?>
