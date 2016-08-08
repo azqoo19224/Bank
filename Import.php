2b12377c77c0a7f0b44250cb5333440b4112a22f
Adam <a19224@yahoo.com.tw>
2016-08-08 16:28:15 +0000

    Import 明細寫入

--- Import.php
+++ Import.php
@@ -2,16 +2,26 @@
 require_once 'DB.php';
 DB::pdoConnect();
 if(isset($_POST["Import"])){
-  
-    $select = DB::$db->prepare("SELECT `balance` FROM `user` WHERE `name` = :name");
+    //
+    $select = DB::$db->prepare("SELECT * FROM `user` WHERE `name` = :name");
     $select->bindParam(":name",$_POST["name"]);
     $select->execute();
     $balance =  $select->fetch(PDO::FETCH_ASSOC);
+    //insert balance
     $insert = DB::$db->prepare("UPDATE `user` SET `balance` = :money WHERE `name` = :name");
     $money=$balance["balance"]+$_POST["moneyin"];
     $insert->bindParam(":money",$money);
     $insert->bindParam(":name",$_POST["name"]);
     $insert->execute();
+    //insert Data
+    $insertData = DB::$db->prepare("INSERT INTO `data` (`id`, `name`, `money`, `project`) VALUES (:id, :name, :money, :project)");
+    $project="使用者:".$balance["name"]."轉入前的金錢為:".$balance["balance"]."<br>轉入後的金錢為:".$money;
+    $insertData->bindParam(":id",$balance["id"]);
+    $insertData->bindParam(":name",$balance["name"]);
+    $insertData->bindParam(":money",$balance["balance"]);
+    $insertData->bindParam(":project",$project);
+    $insertData->execute();
+    
     echo "<script> alert('成功轉入".$_POST["moneyin"]."元'); location.href='bank.php'</script>";
 
 
