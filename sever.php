38e42224576f6cc896e210cd5c500e13edc94478
Adam <a19224@yahoo.com.tw>
2016-08-08 07:28:22 +0000

    sever declare function

--- sever.php
+++ sever.php
@@ -1,5 +1,14 @@
 <?php
-    function import(){
-        echo "OK";
+    if(isset($_POST["btnImport"])){
+        
+    }
+    if(isset($_POST["btnExport"])){
+        
+    }
+    if(isset($_POST["btnCheck"])){
+        
+    }
+    if(isset($_POST["btnDetails"])){
+        
     }
 ?>
