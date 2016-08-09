b0193e60b6e35b8ffafb6caea1e34ccc214e1ca8
Adam <a19224@yahoo.com.tw>
2016-08-09 02:50:41 +0000

    排版

--- Check.php
+++ Check.php
@@ -3,7 +3,7 @@ require_once 'DB.php';
 
 DB::pdoConnect();
 
-if(isset($_POST["Check"])){
+if(isset($_POST["Check"])) {
     $select = DB::$db->prepare("SELECT `balance` FROM `user` WHERE `name` = :name");
     $select->bindParam(":name", $_POST["name"]);
     $select->execute();
