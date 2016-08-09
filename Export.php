622888e9ff5a3e4e2dfee0e80e63982193bba580
Adam <a19224@yahoo.com.tw>
2016-08-09 08:44:34 +0000

    更新變數名

--- Export.php
+++ Export.php
@@ -6,15 +6,15 @@ DB::pdoConnect();
 if (isset($_POST["btnImport"])) {
     try {
         DB::$db->beginTransaction();
-	    
+
         $select = DB::$db->prepare("SELECT * FROM `user` WHERE `name` = :name FOR UPDATE");
         $select->bindParam(":name", $_POST["name"]);
         $select->execute();
         $balance = $select->fetch(PDO::FETCH_ASSOC);
         $insert = DB::$db->prepare("UPDATE `user` SET `balance` = :money WHERE `name` = :name");
         //判斷餘額
-        if($balance["balance"] >= $_POST["moneyOut"]) {
-            $money = $balance["balance"] - $_POST["moneyOut"];
+        if($balance["balance"] >= $_POST["money"]) {
+            $money = $balance["balance"] - $_POST["money"];
             $insert->bindParam(":money", $money);
             $insert->bindParam(":name", $_POST["name"]);
             $insert->execute();
@@ -28,15 +28,15 @@ if (isset($_POST["btnImport"])) {
             $insertData->bindParam(":project", $project);
             $insertData->execute();
 
-            echo "<script> alert('成功轉出" . $_POST["moneyOut"] . "元'); location.href='bank.php'</script>";
+            echo "<script> alert('成功轉出" . $_POST["money"] . "元'); location.href='bank.php'</script>";
 
         } else {
             echo "<script> alert('餘額不足') </script>";
         }
-    
+
         DB::$db->commit();
     	DB::$db = NULL;
-    	
+
     } catch (PDOException $err) {
 	    DB::$db->rollback();
 	    echo "Error: " . $err->getMessage();
@@ -48,13 +48,13 @@ if (isset($_POST["btnImport"])) {
 <html>
     <head>
         <meta charset="UTF-8">
-        
+
     </head>
     <body>
         <form  align="center"  method="post" action="Export.php">
             <input type="text" name="name"/>使用者ID
             <br>
-            <input type="number" min="0" step="1" name="moneyOut"/>轉出金額
+            <input type="number" min="0" step="1" name="money"/>轉出金額
             <br>
             <input type="submit" name="btnImport" value="轉出"/>
             <input type="button" name="back" value="返回" onclick="location.href='bank.php'"/>
--- Import.php
+++ Import.php
@@ -6,31 +6,33 @@ DB::pdoConnect();
 if (isset($_POST["btnImport"])) {
     try {
 	    DB::$db->beginTransaction();
-	    
+
         $select = DB::$db->prepare("SELECT * FROM `user` WHERE `name` = :name FOR UPDATE");
         $select->bindParam(":name", $_POST["name"]);
         $select->execute();
         $balance = $select->fetch(PDO::FETCH_ASSOC);
         //insert balance
- 
+
         $insert = DB::$db->prepare("UPDATE `user` SET `balance` = :money WHERE `name` = :name");
-        $money = $balance["balance"] + $_POST["moneyIn"];
+        $money = $balance["balance"] + $_POST["money"];
         $insert->bindParam(":money", $money);
         $insert->bindParam(":name", $_POST["name"]);
         $insert->execute();
 
-        $insertData = DB::$db->prepare("INSERT INTO `data` (`name`, `money`, `project`) VALUES (:name, :money, :project)");
+        $insertData = DB::$db->prepare("INSERT INTO `data` (`name`, `money`, `project`, `infoAfter`, `infoBefore`) VALUES (:name, :money, :project, :infoAfter, :infoBefore)");
         $project = "使用者:" . $balance["name"] . "<br>轉入前的金錢為:" . $balance["balance"] . "<br>轉入後的金錢為:" . $money;
+        $insertData->bindParam(":infoBefore", $balance["balance"]);
+        $insertData->bindParam(":infoAfter", $money);
         $insertData->bindParam(":name", $balance["name"]);
         $insertData->bindParam(":money", $balance["balance"]);
         $insertData->bindParam(":project", $project);
         $insertData->execute();
 
-        echo "<script> alert('成功轉入" . $_POST["moneyIn"] . "元'); location.href='bank.php'</script>";
+        echo "<script> alert('成功轉入" . $_POST["money"] . "元'); location.href='bank.php'</script>";
 
         DB::$db->commit();
 	    DB::$db = NULL;
-	    
+
     } catch (PDOException $err) {
 	    DB::$db->rollback();
 	    echo "Error: " . $err->getMessage();
@@ -43,13 +45,13 @@ if (isset($_POST["btnImport"])) {
 <html>
     <head>
         <meta charset="UTF-8">
-        
+
     </head>
     <body>
         <form  align="center"  method="post" action="Import.php">
             <input type="text" name="name"/>使用者ID
             <br>
-            <input type="number" min="0" step="1" name="moneyIn"/>轉入金額
+            <input type="number" min="0" step="1" name="money"/>轉入金額
             <br>
             <input type="submit" name="btnImport" value="轉入"/>
             <input type="button" name="back" value="返回" onclick="location.href='bank.php'"/>
