4b32fc9f2aedafe137e45c1b01075ee80b9af97e
Adam <a19224@yahoo.com.tw>
2016-08-11 05:58:52 +0000

    新增 ShowDetails.php

---
+++ ShowDetails.php
@@ -0,0 +1,34 @@
+<?php
+require_once 'DB.php';
+
+DB::pdoConnect();
+
+if (isset($_POST["btnDetails"])) {
+    $select = DB::$db->prepare("SELECT * FROM `data` WHERE `name` = :name");
+    $select->bindParam(":name", $_POST["name"]);
+    $select->execute();
+    $data = $select->fetchAll(PDO::FETCH_ASSOC);
+
+    foreach ($data as $result) {
+        echo "時間:" . $result["time"] . "<br>";
+        echo "使用者:" . $result["name"] . "<br>";
+        echo "選擇操作為__" . $result["info"] . "__金額:" . $result['money'] . "<br>";
+        echo "操作前的金錢為:" . $result["balance"] . "<br>";
+        echo "操作後的金錢為:" .  $result["newBalance"] . "<br><br>";
+    }
+}
+
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <form  align="center"  method="post" action="ShowDetails.php">
+            <input type="text" name="name"/>使用者ID
+            <input type="submit" name="btnDetails" value="查詢"/>
+            <input type="button" name="back" value="返回" onclick="location.href='bank.php'"/>
+        </form>
+    </body>
+</html>
