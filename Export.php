8496b46eba8b710c8f1e8f79bce460d0e505263e
Adam <a19224@yahoo.com.tw>
2016-08-11 07:41:08 +0000

    Export update Optimistic Locking

--- Export.php
+++ Export.php
@@ -4,45 +4,40 @@ require_once 'DB.php';
 DB::pdoConnect();
 
 if (isset($_POST["btnExport"])) {
-    try {
-        DB::$db->beginTransaction();
+    // try {
+        // DB::$db->beginTransaction();
 
-        $select = DB::$db->prepare("SELECT * FROM `user` WHERE `name` = :name FOR UPDATE");
+        $select = DB::$db->prepare("SELECT * FROM `user` WHERE `name` = :name");
         $select->bindParam(":name", $_POST["name"]);
         $select->execute();
         $user = $select->fetch(PDO::FETCH_ASSOC);
 
         if ($user["balance"] >= $_POST["money"]) {
-            $update = DB::$db->prepare("UPDATE `user` SET `balance` = :money WHERE `name` = :name");
+            $update = DB::$db->prepare("UPDATE `user` SET `balance` = :money ,`version` = `version` + 1 WHERE `name` = :name and `version` = :version");
             $count = $user["balance"] - $_POST["money"];
             $update->bindParam(":money", $count);
             $update->bindParam(":name", $_POST["name"]);
+            $update->bindParam(":version", $user['version']);
             $update->execute();
 
-            $insertData = DB::$db->prepare("INSERT INTO `data` (`name`, `balance`, `newBalance`, `info`, `money`) VALUES (:name, :balance, :newBalance, :info, :money)");
-            $info = "轉出:";
-            $insertData->bindParam(":name", $user["name"]);
-            $insertData->bindParam(":newBalance", $count);
-            $insertData->bindParam(":balance", $user["balance"]);
-            $insertData->bindParam(":info", $info);
-            $insertData->bindParam(":money", $_POST["money"]);
-            $insertData->execute();
+            if ($update->rowCount()) {
+                $insertData = DB::$db->prepare("INSERT INTO `data` (`name`, `balance`, `newBalance`, `info`, `money`) VALUES (:name, :balance, :newBalance, :info, :money)");
+                $info = "轉出:";
+                $insertData->bindParam(":name", $user["name"]);
+                $insertData->bindParam(":newBalance", $count);
+                $insertData->bindParam(":balance", $user["balance"]);
+                $insertData->bindParam(":info", $info);
+                $insertData->bindParam(":money", $_POST["money"]);
+                $insertData->execute();
 
-            DB::$db->commit();
-            DB::$db = null;
-
-            echo "<script> alert('成功轉出" . $_POST["money"] . "元'); location.href='bank.php'</script>";
+                echo "<script> alert('成功轉出" . $_POST["money"] . "元'); location.href='bank.php'</script>";
+            } else {
+                 echo "<script> alert('連線逾時 請重試') </script>";
+            }
         } else {
             echo "<script> alert('餘額不足') </script>";
         }
 
-    } catch (PDOException $err) {
-        DB::$db->rollback();
-
-        echo "Error: " . $err->getMessage();
-
-        exit();
-    }
 }
 
 ?>
